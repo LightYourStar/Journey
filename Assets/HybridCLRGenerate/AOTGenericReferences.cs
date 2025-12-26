48d0e45ca1aeb784f86a4e1210ef671df258128f
XingyuHan <272273646@qq.com>
2025-12-26 19:12:33 +0800

    重新规划目录

--- Assets/HybridCLRGenerate/AOTGenericReferences.cs
+++ Assets/HybridCLRGenerate/AOTGenericReferences.cs
@@ -5,6 +5,10 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// {{ AOT assemblies
 	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
 	{
+		"AOT.dll",
+		"UniTask.dll",
+		"UnityEngine.CoreModule.dll",
+		"mscorlib.dll",
 	};
 	// }}
 
@@ -12,9 +16,128 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// }} 
 
 	// {{ AOT generic types
+	// Cysharp.Threading.Tasks.CompilerServices.AsyncUniTask.<>c<JO.SceneMgr.<SwitchToAsync>d__18>
+	// Cysharp.Threading.Tasks.CompilerServices.AsyncUniTask<JO.SceneMgr.<SwitchToAsync>d__18>
+	// Cysharp.Threading.Tasks.ITaskPoolNode<object>
+	// Cysharp.Threading.Tasks.UniTaskCompletionSourceCore<Cysharp.Threading.Tasks.AsyncUnit>
+	// System.Action<float>
+	// System.Action<int>
+	// System.Action<object,object>
+	// System.Action<object>
+	// System.Collections.Generic.ArraySortHelper<int>
+	// System.Collections.Generic.ArraySortHelper<object>
+	// System.Collections.Generic.Comparer<int>
+	// System.Collections.Generic.Comparer<object>
+	// System.Collections.Generic.Dictionary.Enumerator<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.Enumerator<object,int>
+	// System.Collections.Generic.Dictionary.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<object,int>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary.KeyCollection<int,object>
+	// System.Collections.Generic.Dictionary.KeyCollection<object,int>
+	// System.Collections.Generic.Dictionary.KeyCollection<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<object,int>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary.ValueCollection<int,object>
+	// System.Collections.Generic.Dictionary.ValueCollection<object,int>
+	// System.Collections.Generic.Dictionary.ValueCollection<object,object>
+	// System.Collections.Generic.Dictionary<int,UnityEngine.Vector3>
+	// System.Collections.Generic.Dictionary<int,object>
+	// System.Collections.Generic.Dictionary<object,int>
+	// System.Collections.Generic.Dictionary<object,object>
+	// System.Collections.Generic.EqualityComparer<UnityEngine.Vector3>
+	// System.Collections.Generic.EqualityComparer<int>
+	// System.Collections.Generic.EqualityComparer<object>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<int,UnityEngine.Vector3>>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<object,int>>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.ICollection<int>
+	// System.Collections.Generic.ICollection<object>
+	// System.Collections.Generic.IComparer<int>
+	// System.Collections.Generic.IComparer<object>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<int,UnityEngine.Vector3>>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<object,int>>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.IEnumerable<int>
+	// System.Collections.Generic.IEnumerable<object>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<int,UnityEngine.Vector3>>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<object,int>>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.IEnumerator<int>
+	// System.Collections.Generic.IEnumerator<object>
+	// System.Collections.Generic.IEqualityComparer<int>
+	// System.Collections.Generic.IEqualityComparer<object>
+	// System.Collections.Generic.IList<int>
+	// System.Collections.Generic.IList<object>
+	// System.Collections.Generic.KeyValuePair<int,UnityEngine.Vector3>
+	// System.Collections.Generic.KeyValuePair<int,object>
+	// System.Collections.Generic.KeyValuePair<object,int>
+	// System.Collections.Generic.KeyValuePair<object,object>
+	// System.Collections.Generic.List.Enumerator<int>
+	// System.Collections.Generic.List.Enumerator<object>
+	// System.Collections.Generic.List<int>
+	// System.Collections.Generic.List<object>
+	// System.Collections.Generic.ObjectComparer<int>
+	// System.Collections.Generic.ObjectComparer<object>
+	// System.Collections.Generic.ObjectEqualityComparer<UnityEngine.Vector3>
+	// System.Collections.Generic.ObjectEqualityComparer<int>
+	// System.Collections.Generic.ObjectEqualityComparer<object>
+	// System.Collections.ObjectModel.ReadOnlyCollection<int>
+	// System.Collections.ObjectModel.ReadOnlyCollection<object>
+	// System.Comparison<int>
+	// System.Comparison<object>
+	// System.EventHandler<float>
+	// System.Func<System.Threading.Tasks.VoidTaskResult>
+	// System.Func<int>
+	// System.Func<object,System.Threading.Tasks.VoidTaskResult>
+	// System.IProgress<float>
+	// System.Predicate<int>
+	// System.Predicate<object>
+	// System.Progress<float>
+	// System.Runtime.CompilerServices.AsyncTaskMethodBuilder<System.Threading.Tasks.VoidTaskResult>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<System.Threading.Tasks.VoidTaskResult>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<System.Threading.Tasks.VoidTaskResult>
+	// System.Runtime.CompilerServices.TaskAwaiter<System.Threading.Tasks.VoidTaskResult>
+	// System.Threading.Tasks.ContinuationTaskFromResultTask<System.Threading.Tasks.VoidTaskResult>
+	// System.Threading.Tasks.Task<System.Threading.Tasks.VoidTaskResult>
 	// }}
 
 	public void RefMethods()
 	{
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder.AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask.Awaiter,JO.SceneMgr.<SwitchToAsync>d__18>(Cysharp.Threading.Tasks.UniTask.Awaiter&,JO.SceneMgr.<SwitchToAsync>d__18&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder.Start<JO.SceneMgr.<SwitchToAsync>d__18>(JO.SceneMgr.<SwitchToAsync>d__18&)
+		// object JO.ResMgr.LoadAssets<object>(string,JO.ResType)
+		// object JO.ResMgr.LoadGameDataN<object>(string)
+		// object[] System.Array.Empty<object>()
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder.AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter,UIManager.LetterBoardUI.<ShowQuickPassCD>d__52>(System.Runtime.CompilerServices.TaskAwaiter&,UIManager.LetterBoardUI.<ShowQuickPassCD>d__52&)
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder<System.Threading.Tasks.VoidTaskResult>.AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter,UIManager.LetterBoardUI.<ShowQuickPassCD>d__52>(System.Runtime.CompilerServices.TaskAwaiter&,UIManager.LetterBoardUI.<ShowQuickPassCD>d__52&)
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder.Start<UIManager.LetterBoardUI.<ShowQuickPassCD>d__52>(UIManager.LetterBoardUI.<ShowQuickPassCD>d__52&)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder.AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask.Awaiter,JO.SceneMgr.<ChangeScene>d__17>(Cysharp.Threading.Tasks.UniTask.Awaiter&,JO.SceneMgr.<ChangeScene>d__17&)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder.Start<JO.SceneMgr.<ChangeScene>d__17>(JO.SceneMgr.<ChangeScene>d__17&)
+		// object& System.Runtime.CompilerServices.Unsafe.As<object,object>(object&)
+		// System.Void* System.Runtime.CompilerServices.Unsafe.AsPointer<object>(object&)
+		// object UnityEngine.Component.GetComponent<object>()
+		// object UnityEngine.Component.GetComponentInParent<object>()
+		// object[] UnityEngine.Component.GetComponentsInChildren<object>()
+		// object[] UnityEngine.Component.GetComponentsInChildren<object>(bool)
+		// object UnityEngine.GameObject.AddComponent<object>()
+		// object UnityEngine.GameObject.GetComponent<object>()
+		// object UnityEngine.GameObject.GetComponentInChildren<object>()
+		// object UnityEngine.GameObject.GetComponentInChildren<object>(bool)
+		// object[] UnityEngine.GameObject.GetComponentsInChildren<object>(bool)
+		// object UnityEngine.Object.FindFirstObjectByType<object>()
+		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Transform)
+		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Transform,bool)
+		// object UnityEngine.Resources.Load<object>(string)
 	}
 }
